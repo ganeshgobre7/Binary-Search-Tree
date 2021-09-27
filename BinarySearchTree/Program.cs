ddf670b806a7f9effb9ae6095da495205e5acfbe
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-27 15:06:24 +0530

    Creating multile nodes in bst

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -10,6 +10,17 @@ namespace BinarySearchTree
             Tree<int> bst = new Tree<int>(56);
             bst.insert(30);
             bst.insert(70);
+            bst.insert(22);
+            bst.insert(40);
+            bst.insert(60);
+            bst.insert(95);
+            bst.insert(11);
+            bst.insert(65);
+            bst.insert(3);
+            bst.insert(16);
+            bst.insert(63);
+            bst.insert(67);
+
             bst.display();
         }
     }
