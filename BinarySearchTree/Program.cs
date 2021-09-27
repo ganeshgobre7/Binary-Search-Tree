f432974677225dd7c15290d661fd94120bd89be4
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-27 14:53:02 +0530

    Creating BST

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -7,6 +7,10 @@ namespace BinarySearchTree
         static void Main(string[] args)
         {
             Console.WriteLine("WELCOME TO THE BINARY SEARCH TREE PROBLEM!");
+            Tree<int> bst = new Tree<int>(56);
+            bst.insert(30);
+            bst.insert(70);
+            bst.display();
         }
     }
 }
---
+++ BinarySearchTree/Tree.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree
+{
+    class Tree<Gtype> where Gtype:IComparable<Gtype>
+    {
+        public Gtype Data { get; set; }
+        public Tree<Gtype> Left { get; set; }
+        public Tree<Gtype> Right { get; set; }
+
+        public Tree(Gtype data)
+        {
+            this.Data = data;
+            this.Right = null;
+            this.Left = null;
+        }
+
+        public void insert(Gtype data)
+        {
+            Gtype currentNode = this.Data;
+            if((currentNode.CompareTo(data))>0)
+            {
+                if (this.Left == null)
+                    this.Left = new Tree<Gtype>(data);
+                else
+                    this.Left.insert(data);
+            }
+            else
+            {
+                if (this.Right == null)
+                    this.Right = new Tree<Gtype>(data);
+                else
+                    this.Right.insert(data);
+            }
+        }
+        private int l_Count = 0;
+        private int r_Count = 0;
+
+        public void display()
+        {
+            if(this.Left!=null)
+            {
+                this.l_Count++;
+                this.Left.display();
+            }
+            Console.WriteLine(this.Data.ToString());
+            if(this.Right!=null)
+            {
+                this.r_Count++;
+                this.Right.display();
+            }
+        }
+
+    }
+}
