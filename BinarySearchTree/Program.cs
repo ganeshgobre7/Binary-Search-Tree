f52af8cc96558456c850b34ca5f79cf34765681d
ganeshgobre7 <ganeshgobre7@gmail.com>
2021-09-27 13:36:08 +0530

    Welcome message added

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO THE BINARY SEARCH TREE PROBLEM!");
+        }
+    }
+}
